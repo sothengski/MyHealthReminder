af48ebac58cae24c1279fe86a0a776c117b2cfcd
Chheang Sotheng <chheangsotheng@yahoo.com>
2024-07-03 12:04:49 -0700

    Passing/Receiving Data between Main Activity & Detail Activity

--- app/src/main/java/com/example/myhealthreminder/DetailActivity.kt
+++ app/src/main/java/com/example/myhealthreminder/DetailActivity.kt
@@ -16,55 +16,28 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // enable the back button in the navigation bar
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-
         enableEdgeToEdge()
         setContentView(R.layout.activity_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-    }
+        }
 
-
-
-
-
-
-
-
-
-
-//        // calling the action bar
-//        var actionBar = getSupportActionBar()
-//        // showing the back button in action bar
-//        if (actionBar != null) {
-//            actionBar.setDisplayHomeAsUpEnabled(true)
-//        }
-//
-//        // this event will enable the back
-//        // function to the button on press
-//        override fun onContextItemSelected(item: MenuItem): Boolean {
-//            when (item.itemId) {
-//                android.R.id.home -> {
-//                    finish()
-//                    return true
-//                }
-//            }
-//            return super.onContextItemSelected(item)
-//        }
-
-
-
+        // enable the back button in the navigation bar
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         val imageView: ImageView = findViewById(R.id.imageView);
         val dTitle: TextView = findViewById(R.id.dTitle);
         val dDescription: TextView = findViewById(R.id.dDescription);
 
-//        Bundle bundle = getIntent().getExtras();
-//        imageView.setImageResource(bundle.getInt("image"));
-//        dTitle.setText(bundle.getString("title"));
-//        dDescription.setText(bundle.getString("description"));
+        // Get the data from the intent
+        val bundle = intent.extras;
+        if (bundle != null){
+            // Get the data from the intent
+            imageView.setImageResource(bundle.getInt("image"))
+            dTitle.setText(bundle.getString("title"))
+            dDescription.setText(bundle.getString("description"))
+        }
     }
 }
